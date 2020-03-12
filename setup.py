8d39b87ad92f188bbdfbbe20e72e903356047530
nmoore32 <61410102+nmoore32@users.noreply.github.com>
2020-03-12 02:48:56 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-import cx_Freeze
-executables = [cx_Freeze.Executable("alien_invasion.py")]
-cx_Freeze.setup(
-    name="Alien Invasion",
-    options={"build_exe": {"packages": ["pygame"],
-                           "include_files": ["images/alien1a.bmp", "images/alien1b.bmp", "images/shipa.bmp",
-                                             "sound_effects/explosion.wav", "sound_effects/game_over.wav",
-                                             "sound_effects/game_start.wav", "sound_effects/laser.wav",
-                                             "sound_effects/lose_life.wav"]}},
-    executables=executables
-)
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(name="Alien Invasion",
+      version="0.9",
+      description="Space Invaders Clone",
+      options={"build_exe": {"packages": ["pygame", "os"],
+                             "excludes": ["tkinter", "numpy", "test", "distutils"],
+                             "include_files": ["images/alien1a.bmp", "images/alien1b.bmp", "images/shipa.bmp",
+                                               "sound_effects/explosion.wav", "sound_effects/game_over.wav",
+                                               "sound_effects/game_start.wav", "sound_effects/laser.wav",
+                                               "sound_effects/lose_life.wav"]}},
+      executables=[Executable("alien_invasion.py", base=base)]
+      )
